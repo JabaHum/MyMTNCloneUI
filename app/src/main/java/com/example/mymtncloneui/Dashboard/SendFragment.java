bf6e09451c67b76497ef39c4b768e2f1f1610142
JabaHum <jabajnan8@gmail.com>
2020-05-20 18:08:15 +0300

    InitialCommit

--- app/src/main/java/com/example/mymtncloneui/Dashboard/SendFragment.java
+++ app/src/main/java/com/example/mymtncloneui/Dashboard/SendFragment.java
@@ -1,6 +1,34 @@
 package com.example.mymtncloneui.Dashboard;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.mymtncloneui.R;
+
 public class SendFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.sendfragment, container, false);
+
+        return view;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
 }
