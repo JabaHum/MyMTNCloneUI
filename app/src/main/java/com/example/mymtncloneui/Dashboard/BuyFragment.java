a0805d4da30ffd3ecf3bc4204a77b0c3fe52974e
JabaHum <jabajnan8@gmail.com>
2020-05-20 17:51:57 +0300

    InitialCommit

--- app/src/main/java/com/example/mymtncloneui/Dashboard/BuyFragment.java
+++ app/src/main/java/com/example/mymtncloneui/Dashboard/BuyFragment.java
@@ -1,7 +1,34 @@
 package com.example.mymtncloneui.Dashboard;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class BuyFragment extends Fragment {
+import com.example.mymtncloneui.R;
 
+public class BuyFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.buyfragment, container, false);
+
+        return view;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
 }
